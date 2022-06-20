e8aade1e522dcfc9cb7fee1275ff643398a5d842
Samy OUHIB <samy.ouhib@epita.fr>
2022-06-20 21:05:48 +0200

    implemetend all feature classes without exec

---
+++ src/main/java/fr/epita/assistants/features/Install.java
@@ -0,0 +1,17 @@
+package fr.epita.assistants.features;
+
+import fr.epita.assistants.myide.domain.entity.Feature;
+import fr.epita.assistants.myide.domain.entity.Mandatory;
+import fr.epita.assistants.myide.domain.entity.Project;
+
+public class Install implements Feature {
+    @Override
+    public ExecutionReport execute(Project project, Object... params) {
+        return null;
+    }
+
+    @Override
+    public Type type() {
+        return Mandatory.Features.Maven.INSTALL;
+    }
+}
