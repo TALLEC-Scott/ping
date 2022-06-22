935b5c58a3800d5779f946a48dd13062ec53b0cb
Scott TALLEC <scott.tallec@epita.fr>
2022-06-22 20:38:30 +0200

    feat(Clean)

--- src/main/java/fr/epita/assistants/features/Clean.java
+++ src/main/java/fr/epita/assistants/features/Clean.java
@@ -2,9 +2,11 @@ package fr.epita.assistants.features;
 
 import fr.epita.assistants.myide.domain.entity.Feature;
 import fr.epita.assistants.myide.domain.entity.Mandatory;
+import fr.epita.assistants.myide.domain.entity.Node;
 import fr.epita.assistants.myide.domain.entity.Project;
 import org.apache.maven.Maven;
 
+import java.io.File;
 import java.io.IOException;
 
 public class Clean implements Feature {
@@ -16,34 +18,34 @@ public class Clean implements Feature {
         }
     };
     public ExecutionReport execute(Project project, Object... params) {
+        var root = project.getRootNode().getPath().toAbsolutePath();
         Process pr;
         boolean isSucess = true;
-        int exitValue = -1;
-        //var root_path = Project.getRootNode()
+        int exitValue;
+
+
 
 
         try {
-            Runtime.getRuntime().exec("cd");
-            pr = Runtime.getRuntime().exec("mvn clean install");
+            pr = Runtime.getRuntime().exec("mvn clean", null, new File(root.toString()) );
              exitValue =pr.waitFor();
              isSucess = (exitValue == 0);
 
-        } catch (IOException e) {
+        } catch (IOException | InterruptedException e) {
 
             isSucess = false;
             //throw new RuntimeException(e);
-        } catch (InterruptedException e) {
-            isSucess = false;
-            //throw new RuntimeException(e);
+        } finally {
+            boolean finalIsSucess = isSucess;
+            ExecutionReport report = new ExecutionReport() {
+                @Override
+                public boolean isSuccess() {
+                    return finalIsSucess;
+                }
+            };
         }
-        boolean finalIsSucess = isSucess;
-        ExecutionReport report = new ExecutionReport() {
-            @Override
-            public boolean isSuccess() {
-                return finalIsSucess;
-            }
-        };
         return report;
+
     }
 
     @Override
