5101104a136b620565d4e21920cbcce82c1a1dbe
voTeXStudios <62063018+voTeXStudios@users.noreply.github.com>
2022-06-25 15:22:50 +0200

    feat(feat(Commit.java) Appended the messages with newlines and have empty deafault message)

--- src/main/java/fr/epita/assistants/features/Commit.java
+++ src/main/java/fr/epita/assistants/features/Commit.java
@@ -19,11 +19,16 @@ public class Commit implements Feature {
             return () -> false;
         }
 
+        var msg = "";
+
+        for (var str : params){
+            msg += "\n\n" + params;
+        }
         try {
+            git.commit().setMessage(msg).call();
             for (var str : params){
                 git.commit().setMessage((String) str);
             }
-            git.commit().call();
         } catch (GitAPIException e) {
             e.printStackTrace();
             return () -> false;
